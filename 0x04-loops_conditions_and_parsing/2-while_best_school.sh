ef2152aa97860e0c35c506555477602cfdfd35be
Lawal Olanrewaju <lawallanre.abbahouse@gmail.com>
2024-02-28 18:33:17 +0100

    test

---
+++ 0x04-loops_conditions_and_parsing/2-while_best_school.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# Write a Bash script that displays Best School 10 times. You must use the while loop (for and until are forbidden)
+i=0
+while [ $i -lt 10 ]
+do
+    echo "Best Shcool"
+    i=$((i+1))
+done
